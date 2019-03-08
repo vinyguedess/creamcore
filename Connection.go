554faca5ac15d83e61ed01a79a24b984cd6283e0
Vinicius Guedes <viniciusgued@gmail.com>
2019-03-08 13:45:55 -0300

    style: comments code for matching pattern

--- Connection.go
+++ Connection.go
@@ -2,7 +2,7 @@ package creamcore
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // Imports SQLite parser
 )
 
 var conn *gorm.DB
@@ -16,6 +16,8 @@ func makeConnection() {
 	conn = db
 }
 
+// GetConnection responsible for generate a connection if necessary and return
+// its object
 func GetConnection() *gorm.DB {
 	if conn == nil {
 		makeConnection()
