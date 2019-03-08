9e45b37b80a8df8eccc6e9a2fe7b8d259c56235b
Vinicius Guedes <viniciusgued@gmail.com>
2019-03-08 15:19:18 -0300

    refactor: database connection management

--- Connection.go
+++ Connection.go
@@ -1,14 +1,58 @@
 package creamcore
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite" // Imports SQLite parser
+	"github.com/joho/godotenv"
 )
+
+// ConnectionAuth holds and treats database connection
+type ConnectionAuth struct {
+	Driver string
+	Host   string
+	Port   int64
+	Name   string
+	User   string
+	Pass   string
+}
+
+// GetDriver returns connection driver
+func (auth *ConnectionAuth) GetDriver() string {
+	if auth.Driver == "sqlite" {
+		return "sqlite3"
+	}
+
+	return auth.Driver
+}
+
+// GetConnString gets connection string formatted for GORM
+func (auth *ConnectionAuth) GetConnString() string {
+	if auth.Driver == "sqlite" {
+		return auth.Name
+	} else if auth.Driver == "mysql" {
+		return auth.User + ":" + auth.Pass + "@/" + auth.Name + "?charset=utf8&parseTime=True&loc=Local"
+	}
+
+	return ""
+}
 
 var conn *gorm.DB
 
 func makeConnection() {
-	db, err := gorm.Open("sqlite3", "creamcore.sqlite3")
+	godotenv.Load()
+	port, _ := strconv.ParseInt(os.Getenv("DB_PORT"), 10, 0)
+	auth := ConnectionAuth{
+		Driver: os.Getenv("DB_DRIVER"),
+		Host:   os.Getenv("DB_HOST"),
+		Port:   port,
+		Name:   os.Getenv("DB_NAME"),
+		User:   os.Getenv("DB_USER"),
+		Pass:   os.Getenv("DB_PASS"),
+	}
+	db, err := gorm.Open(auth.GetDriver(), auth.GetConnString())
 	if err != nil {
 		panic(err)
 	}
--- Connection_test.go
+++ Connection_test.go
@@ -19,7 +19,54 @@ func (m *UserTest) TableName() string {
 
 func TestConnectingToDatabase(t *testing.T) {
 	conn := creamcore.GetConnection()
+	conn.DropTableIfExists(&UserTest{})
 	conn.CreateTable(&UserTest{})
 
 	assert.True(t, conn.HasTable("users"))
+}
+
+func TestConnectionAuthGetDriverSqlite(t *testing.T) {
+	auth := creamcore.ConnectionAuth{
+		Driver: "sqlite",
+	}
+
+	assert.Equal(t, auth.GetDriver(), "sqlite3")
+}
+
+func TestConnectionAuthGetDriverOthers(t *testing.T) {
+	auth := creamcore.ConnectionAuth{
+		Driver: "mysql",
+	}
+
+	assert.Equal(t, auth.GetDriver(), "mysql")
+}
+
+func TestConnectionAuthGetConnStringSqlite(t *testing.T) {
+	auth := creamcore.ConnectionAuth{
+		Driver: "sqlite",
+		Name:   "anydb.sqlite",
+	}
+
+	assert.Equal(t, auth.GetConnString(), "anydb.sqlite")
+}
+
+func TestConnectionAuthGetConnStringMySQL(t *testing.T) {
+	auth := creamcore.ConnectionAuth{
+		Driver: "mysql",
+		Host:   "localhost",
+		Port:   3306,
+		Name:   "anydb",
+		User:   "root",
+		Pass:   "123321",
+	}
+
+	assert.Equal(t, auth.GetConnString(), "root:123321@/anydb?charset=utf8&parseTime=True&loc=Local")
+}
+
+func TestConnectionAuthGetConnStringOthers(t *testing.T) {
+	auth := creamcore.ConnectionAuth{
+		Driver: "unknown",
+	}
+
+	assert.Equal(t, auth.GetConnString(), "")
 }
