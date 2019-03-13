16605d4933141abc45de489954f30fa0085fc364
Vinicius Guedes <viniciusgued@gmail.com>
2019-03-13 11:00:57 -0300

    style: adds comments declaring struct and methods information

--- Application.go
+++ Application.go
@@ -8,29 +8,37 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// MuxRequestHandler defines Mux Request Handler typo
 type MuxRequestHandler func(http.ResponseWriter, *http.Request)
+
+// RequestHandler defines CReaM Request Handler typo
 type RequestHandler func(*http.Request) (int, string)
 
-type Application struct {
-	Name   string
-	Router *mux.Router
-}
-
-func (a *Application) Register(route string, handler RequestHandler, method ...string) *Application {
-	a.Router.HandleFunc(route, a.parseResponse(handler)).Methods(method...)
-	return a
-}
-
-func (a *Application) Run(port int) {
-	server := http.ListenAndServe(":"+strconv.Itoa(port), a.Router)
-	log.Fatal(server)
-}
-
+// NewApplication is a Singleton responsible of returning Application instance
 func NewApplication(name string) *Application {
 	app := new(Application)
 	app.init(name)
 
 	return app
+}
+
+// Application is the core class responsible for registering handlers,
+// middlewares and start up the app
+type Application struct {
+	Name   string
+	Router *mux.Router
+}
+
+// Register is the method responsible of registering middlewares
+func (a *Application) Register(route string, handler RequestHandler, method ...string) *Application {
+	a.Router.HandleFunc(route, a.parseResponse(handler)).Methods(method...)
+	return a
+}
+
+// Run is the method responsible of  starting up the application server
+func (a *Application) Run(port int) {
+	server := http.ListenAndServe(":"+strconv.Itoa(port), a.Router)
+	log.Fatal(server)
 }
 
 func (a *Application) init(name string) {
